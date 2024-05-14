98dc8d90183a957e31baf63e2502bd6c75f412dc
oxananazaruk <kurylooxana99@gmail.com>
2024-05-14 18:05:55 +0300

    update env

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,19 @@
 import { initializeApp } from 'firebase/app';
 
 const VITE_API = import.meta.env.VITE_API;
-const VITE_AUTHDOMAIN = import.meta.env.VITE_AUTHDOMAIN;
-const VITE_DATABASEURL = import.meta.env.VITE_DATABASEURL;
-const VITE_APPID = import.meta.env.VITE_APPID;
+// const VITE_AUTHDOMAIN = import.meta.env.VITE_AUTHDOMAIN;
+// const VITE_DATABASEURL = import.meta.env.VITE_DATABASEURL;
+// const VITE_APPID = import.meta.env.VITE_APPID;
 
 const firebaseConfig = {
   apiKey: VITE_API,
-  authDomain: VITE_AUTHDOMAIN,
-  databaseURL: VITE_DATABASEURL,
+  authDomain: 'nanny-services-61617.firebaseapp.com',
+  databaseURL:
+    'https://nanny-services-61617-default-rtdb.europe-west1.firebasedatabase.app',
   projectId: 'nanny-services-61617',
   storageBucket: 'nanny-services-61617.appspot.com',
   messagingSenderId: '38815472902',
-  appId: VITE_APPID,
+  appId: '1:38815472902:web:521b7836157eec4af45287',
 };
 
 export const app = initializeApp(firebaseConfig);
